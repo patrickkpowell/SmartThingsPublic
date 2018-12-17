a2caf65253dac1fd1d302db1ab8812ea73364ddb
patrickkpowell <patrick@powellcompanies.com>
2018-12-17 01:03:03 -0600

    Giving credit

--- devicetypes/patrickkpowell/Shelly/shelly-1or2.groovy
+++ devicetypes/patrickkpowell/Shelly/shelly-1or2.groovy
@@ -12,6 +12,8 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *  Thanks bigjuanpa for your contributions
+ *
  */
 metadata {
   definition (name: "Shelly1or2", namespace: "patrickkpowell", author: "Patrick Powell") {
